66be991084b5c706e774b35aeb97702ecda3ba05
Vladislav <klyaginvladislav@mail.ru>
2022-06-12 22:47:17 +0300

    1.8.1

--- src/Main.java
+++ src/Main.java
@@ -11,36 +11,42 @@ public class Main {
     }
 
     public static void checkYear(int year) {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " высокосный");
-        else
+        }else {
             System.out.println(year + " не высокосный");
+        }
 
     }
 
     public static void checkPhone(int oc, int year) {
         int currentYear = LocalDate.now().getYear();
 
-        if (oc == 0 && year >= currentYear)
+        if (oc == 0 && year >= currentYear) {
             System.out.println(" У вас актуальная IOS");
-        else if (oc == 0 && year < currentYear)
+        }else if (oc == 0 && year < currentYear) {
             System.out.println(" У вас старая версия IOS обновите!");
-        else if (oc == 1 && year < currentYear)
+        }else if (oc == 1 && year < currentYear) {
             System.out.println(" У вас старая версия ANDROID обновите!");
-        else
+        }else {
             System.out.println(" Я не знаю какая у вас операционная система");
+        }
     }
 
     public static int checkPatch(int distance) {
         int days = 0;
-        if (distance >= 0 && distance < 40)
+        if (distance >= 0 && distance < 40){
             days += 1;
-        if (distance > 40 && distance < 60)
+        }
+        if (distance > 40 && distance < 60) {
             days += 2;
-        if (distance > 60 && distance < 80)
+        }
+        if (distance > 60 && distance < 80) {
             days += 3;
-        if (distance >= 80)
+        }
+        if (distance >= 80) {
             days += 4;
+        }
         return days;
     }
 }
